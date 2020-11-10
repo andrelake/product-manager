8d206f82c80ad053e107cca6bf466e4a96dd7112
André Lago <andre.lake@hotmail.com>
2020-11-10 12:44:35 -0300

    Navbar e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 import { ProductListComponent } from './products/product-list.component';
 import { StarComponent } from './stars/star.component';
+import { NavBarComponent } from './navbar/nav-bar.component';
+import { PageNotFoundComponent } from './pageNotFound/pageNotFound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    StarComponent
+    StarComponent,
+    NavBarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'products', pathMatch: 'full'
+      },
+      {
+        path: 'products', component: ProductListComponent
+      },
+      {
+        path: '**', component: PageNotFoundComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -3,7 +3,6 @@ import { Product } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-    selector: 'product-list',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
